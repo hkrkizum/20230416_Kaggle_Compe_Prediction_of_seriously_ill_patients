143f4a3955e7bb5a9ab5189201df526871a2361d
hkrkizum <aoxora333@gmail.com>
2023-04-16 12:51:15 +0900

    update

--- _targets.R
+++ _targets.R
@@ -1,9 +1,12 @@
+# 0. load package -------------------------
 library(targets)
 
+
+# 1. set optins --------------------------------------------------------------
 options(tidyverse.quiet = TRUE)
 tar_option_set(
   packages = c("tidyverse",
-               "readxl",
+               "data.table",
                "here",
                "skimr",
                "qs"),
@@ -11,11 +14,45 @@ tar_option_set(
   seed = 54147
 )
 
-# Set target-specific options such as packages:
-# tar_option_set(packages = "utils") # nolint
+# 2. set functions ----------------------------------------------------------
 
-# End this file with a list of target objects.
+
+# 3. define pipeline --------------------------------------------------------
 list(
-  tar_target(data, data.frame(x = sample.int(100), y = sample.int(100))),
-  tar_target(summary, summ(data)) # Call your custom functions as needed.
+  tar_target(
+    name = in_f_train,
+    command = {
+      here::here("Rawdata", "train_df.csv")
+    }
+  ),
+  tar_target(
+    name = in_f_test,
+    command = {
+      here::here("Rawdata", "test_df.csv")
+    }
+  ),
+  tar_target(
+    name = in_f_submitt,
+    command = {
+      here::here("Rawdata", "submission.csv")
+    }
+  ),
+  tar_target(
+    name = df_train,
+    command = {
+      data.table::fread(in_f_train)
+    }
+  ),
+  tar_target(
+    name = df_test,
+    command = {
+      data.table::fread(in_f_test)
+    }
+  ),
+  tar_target(
+    name = df_submitt,
+    command = {
+      data.table::fread(in_f_submitt)
+    }
+  )
 )
